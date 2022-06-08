556ea20d1b96058ab6f5095a60d9daedfbea912c
ahmadmm01 <skyahmad74@gmail.com>
2022-06-08 16:16:49 +0700

    Add project files.

---
+++ ConnectedEnvironment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
